c549a46407503415dcf4082f218af44f74596e5b
Hannah <hannahvanseeters@gmail.com>
2019-01-09 15:57:40 -0800

    Added Login Component and added it to Welcome Screen

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,21 +1,113 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput } from 'react-native';
-import { Button, Card, CardSection, Input, Spinner } from './common';
-
-// just putting secureTextEntry without equaling boolean
-// it will automatically be true
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Image,
+  Alert
+} from 'react-native';
+import Icon from 'react-native-ionicons';
 
 class LoginForm extends Component {
+
+  constructor(props) {
+    super(props);
+    state = {
+      email   : '',
+      password: '',
+    }
+  }
+
+  onClickListener = (viewId) => {
+    Alert.alert("Alert", "Button pressed "+viewId);
+  }
+
   render() {
     return (
       <View>
-        <Text>Login Here</Text>
-        <TextInput />
-        <TextInput />
+        <View style={styles.inputContainer}>
+          <Icon name="mail" size={30} color="black" style={styles.inputIcon} />
+          <TextInput style={styles.inputs}
+              placeholder="Email"
+              keyboardType="email-address"
+              underlineColorAndroid='transparent'
+              onChangeText={(email) => this.setState({email})}/>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Icon name="key" size={30} color="black" style={styles.inputIcon} />
+          <TextInput style={styles.inputs}
+              placeholder="Password"
+              secureTextEntry={true}
+              underlineColorAndroid='transparent'
+              onChangeText={(password) => this.setState({password})}/>
+        </View>
+
+        <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.props.navigation.navigate('Home')}>
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('restore_password')}>
+            <Text>Forgot your password?</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('register')}>
+            <Text>Register</Text>
+        </TouchableHighlight>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCDCDC',
+  },
+  inputContainer: {
+      borderBottomColor: 'gray',
+      backgroundColor: '#FFFFFF',
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width:250,
+      height:45,
+      marginBottom:20,
+      flexDirection: 'row',
+      alignItems:'center'
+  },
+  inputs:{
+      height:45,
+      marginLeft:16,
+      borderBottomColor: '#1E1E1E',
+      flex:1,
+  },
+  inputIcon:{
+    width:30,
+    height:30,
+    marginLeft:15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+  },
+  loginButton: {
+    backgroundColor: "tomato",
+  },
+  loginText: {
+    color: 'white',
+    fontWeight: 'bold',
+  }
+});
 
 export default LoginForm;
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -9,10 +9,7 @@ class WelcomeScreen extends React.Component {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Image source={require('../assets/food-cycle.png')} />
-        <Button
-          onPress={() => this.props.navigation.navigate('Home')}
-        >Login</Button>
-        <Button onPress={() => alert('button pressed')}>Sign Up</Button>
+        <LoginForm navigation={this.props.navigation}/>
       </View>
     );
   }
