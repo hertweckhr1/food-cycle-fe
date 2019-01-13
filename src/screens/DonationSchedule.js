d71b5f159d890f902df8b512dc5f4f99bb2d7a21
Hannah <hannahvanseeters@gmail.com>
2019-01-12 22:59:24 -0800

    format for doner setting tab complete

--- src/screens/DonationSchedule.js
+++ src/screens/DonationSchedule.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
-
+import { Container, Button, Thumbnail, Body, Left, Right, Content, List, ListItem, Text } from 'native-base';
 class DonationSchedule extends Component {
   render() {
-    return(
-      <View>
-        <Text>Donations go here!</Text>
-      </View>
-    )
+    return (
+      <Container>
+        <Content>
+          <List>
+            <ListItem thumbnail>
+              <Left>
+                <Text>4/12/19</Text>
+              </Left>
+              <Body>
+                <Text>Sankhadeep</Text>
+                <Text note numberOfLines={1}>Its time to build a difference . .</Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+    );
   }
 }
-
 export { DonationSchedule }
--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,16 +1,120 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import Icon from 'react-native-ionicons'
+import { ScrollView, StyleSheet } from 'react-native'
+import { Container, Left, Right, Content, ListItem, Text, Separator } from 'native-base';
 
 class Settings extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-        <Icon name="settings" size={40} color="green"/>
-      </View>
+      <ScrollView>
+        <Container>
+          <Content>
+            <Separator bordered>
+              <Text>MY INFO</Text>
+            </Separator>
+            <ListItem last>
+              <Left>
+                <Text style={styles.fieldText}>Company Name</Text>
+              </Left>
+              <Right>
+                <Text style={styles.personalText}>Central Coop</Text>
+              </Right>
+            </ListItem>
+            <ListItem>
+              <Left>
+                <Text style={styles.fieldText}>Point of Contact</Text>
+              </Left>
+              <Right>
+                <Text style={styles.personalText}>Theresa Redmond</Text>
+              </Right>
+            </ListItem>
+            <ListItem>
+              <Left>
+                <Text style={styles.fieldText}>Email</Text>
+              </Left>
+              <Right>
+                <Text style={styles.personalText}>Theresa@centralcoop.coop</Text>
+              </Right>
+            </ListItem>
+            <ListItem>
+              <Left>
+                <Text style={styles.fieldText}>User Status</Text>
+              </Left>
+              <Right>
+                <Text style={styles.personalText}>Doner</Text>
+              </Right>
+            </ListItem>
+            <Separator bordered>
+              <Text>ADDRESS</Text>
+            </Separator>
+            <ListItem>
+              <Left>
+                <Text style={styles.fieldText}>Street Address</Text>
+              </Left>
+              <Right>
+                <Text style={styles.personalText}>1600 E Madison St</Text>
+              </Right>
+            </ListItem>
+            <ListItem>
+              <Left>
+                <Text style={styles.fieldText}>Street Address2</Text>
+              </Left>
+              <Right>
+                <Text style={styles.personalText}>--</Text>
+              </Right>
+            </ListItem>
+            <ListItem>
+              <Left>
+                <Text style={styles.fieldText}>City</Text>
+              </Left>
+              <Right>
+                <Text style={styles.personalText}>Seattle</Text>
+              </Right>
+            </ListItem>
+            <ListItem>
+              <Left>
+                <Text style={styles.fieldText}>State</Text>
+              </Left>
+              <Right>
+                <Text style={styles.personalText}>WA</Text>
+              </Right>
+            </ListItem>
+            <ListItem>
+              <Left>
+                <Text style={styles.fieldText}>Zip</Text>
+              </Left>
+              <Right>
+                <Text style={styles.personalText}>98122</Text>
+              </Right>
+            </ListItem>
+            <Separator bordered>
+                <Text>PASSWORD</Text>
+            </Separator>
+            <ListItem>
+              <Text style={styles.passwordText}>Change Password</Text>
+            </ListItem>
+            <Separator bordered>
+            </Separator>
+          </Content>
+        </Container>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  fieldText: {
+    fontFamily: 'Futura',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  personalText: {
+    fontFamily: 'Futura'
+  },
+  passwordText: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+    fontFamily: 'Futura',
+  }
+})
 
 export { Settings };
