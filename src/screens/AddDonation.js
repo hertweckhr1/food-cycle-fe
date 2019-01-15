4cf3c8fad95c710f4681ca8ff99443792b2d2ae2
Hannah <hannahvanseeters@gmail.com>
2019-01-14 21:42:50 -0800

    form looking good for donations

--- src/screens/AddDonation.js
+++ src/screens/AddDonation.js
@@ -9,9 +9,13 @@ import {
   TouchableOpacity,
   Alert,
   ScrollView,
+  Picker
 } from 'react-native';
 import axios from 'axios';
 import DateTimePicker from 'react-native-modal-datetime-picker';
+import Moment from 'react-moment';
+import Icon from 'react-native-ionicons';
+import ModalDropdown from 'react-native-modal-dropdown';
 
 
 class AddDonation extends Component {
@@ -23,22 +27,43 @@ class AddDonation extends Component {
     measurement: '',
     quantity: '',
     pickupDetails: '',
-    pickupStartTime: '2019-05-15',
+    pickupStartTime: '',
     pickupEndTime: '',
     error: '',
-    isStartTimePickerVisible: 'false',
+    isStartTimePickerVisible: false,
+    isEndTimePickerVisible: false,
   };
 
-  handleStartDatePicked = (date) => {
-    console.log('A date has been picked:');
+  handleStartDatePicked = (datetime) => {
+    // console.log('A date has been picked:');
+    // console.log(datetime.toString());
+    // console.log(typeof datetime)
+    const newDate = new Date(datetime)
+    // console.log(newDate);
+    // const formattedDate = datetime.toString()
     this.setState({
       isStartTimePickerVisible: false,
+      pickupStartTime: newDate
     })
+    // console.log(this.state.pickupStartTime);
+    // console.log(this.state.isStartTimePickerVisible)
   };
 
   showStartTimePicker = () => this.setState({ isStartTimePickerVisible: true });
-
   hideStartTimePicker = () => this.setState({ isStartTimePickerVisible: false });
+
+  handleEndDatePicked = (datetime) => {
+    const newDate = new Date(datetime)
+    this.setState({
+      isEndTimePickerVisible: false,
+      pickupEndTime: newDate
+    })
+    // console.log(this.state.pickupStartTime);
+    // console.log(this.state.isStartTimePickerVisible)
+  };
+
+  showEndTimePicker = () => this.setState({ isEndTimePickerVisible: true });
+  hideEndTimePicker = () => this.setState({ isEndTimePickerVisible: false });
 
 
   onClickListener = (viewId) => {
@@ -81,57 +106,92 @@ class AddDonation extends Component {
         <View style={styles.container}>
           <Text style={styles.title}>Add Donation</Text>
           <View style={styles.inputContainer}>
-            <TextInput style={styles.inputs}
-                placeholder="Product Type"
-                underlineColorAndroid='transparent'
-                onChangeText={productType => this.setState({ productType })}/>
+            <Text style={styles.dateText}>Product Type</Text>
+            <ModalDropdown
+              textStyle={{color: 'gray', padding: 30, fontFamily: 'Futura'}}
+              dropdownTextStyle={{color: 'black', backgroundColor: '#9ACD32',
+                width:80, textAlign: 'center', fontFamily: 'Futura'}}
+              dropdownTextHighlightStyle={{backgroundColor: 'tomato'}}
+              onSelect={ productType => this.setState({ productType })}
+              options={["Meat or Fish", "Produce", "Dairy", "Beverages",
+              "Chilled or Frozen", "Shelf Stable"]}
+            />
           </View>
+
           <View style={styles.inputContainer}>
+            <Text style={styles.dateText}>Product Description</Text>
             <TextInput style={styles.inputs}
                 placeholder="Product Description"
                 underlineColorAndroid='transparent'
                 onChangeText={ productDescription => this.setState({ productDescription })}/>
           </View>
+
           <View style={styles.inputContainer}>
-            <TextInput style={styles.inputs}
-                placeholder="Measurement"
-                underlineColorAndroid='transparent'
-                onChangeText={measurement => this.setState({ measurement })}/>
+            <Text style={styles.dateText}>Measurement</Text>
+            <ModalDropdown
+              textStyle={{color: 'gray', padding: 30, fontFamily: 'Futura'}}
+              dropdownTextStyle={{color: 'black', backgroundColor: '#9ACD32',
+                width:80, textAlign: 'center', fontFamily: 'Futura'}}
+              dropdownTextHighlightStyle={{backgroundColor: 'tomato'}}
+              onSelect={measurement => this.setState({ measurement })}
+              options={["lb", "crates", "liters", "gallons", "bags", "pieces"]}
+            />
           </View>
+
           <View style={styles.inputContainer}>
+            <Text style={styles.dateText}>Quantity</Text>
             <TextInput style={styles.inputs}
                 placeholder="Quantity"
                 underlineColorAndroid='transparent'
                 onChangeText={quantity => this.setState({ quantity })}/>
           </View>
+
           <View style={styles.inputContainer}>
+            <Text style={styles.dateText}>Pickup Details</Text>
             <TextInput style={styles.inputs}
                 placeholder="Pick Up Details"
                 underlineColorAndroid='transparent'
                 onChangeText={pickupDetails => this.setState({ pickupDetails })}/>
           </View>
-          <View style={styles.inputContainer}>
-            <Text>{this.state.pickupStartTime}</Text>
-            <TouchableOpacity style={styles.button} onPress={this.showStartTimePicker}>
+          <View style={styles.outsideDateContainer}>
+            <View style={styles.dateContainer}>
               <Text style={styles.dateText}>Start Time</Text>
-            </TouchableOpacity>
-            <DateTimePicker
-              cancelTextIOS={'Exit'}
-              confirmTextIOS={'OK'}
-              cancelTextStyle={{color: 'red', fontSize: 20}}
-              confirmTextStyle={{color: 'blue', fontSize: 20}}
-              isVisible={this.state.isStartTimePickerVisible}
-              onConfirm={this.handleStartDatePicked}
-              onCancel={this.hideStartTimePicker}
-              mode={'datetime'}
-              is24Hour={true}
-            />
-          </View>
-          <View style={styles.inputContainer}>
-            <TextInput style={styles.inputs}
-                placeholder="Pick Up End Time"
-                underlineColorAndroid='transparent'
-                onChangeText={pickupEndTime => this.setState({ pickupEndTime })}/>
+              <Text style={styles.dateInnerText}>{this.state.pickupStartTime.toString().slice(4,21)}</Text>
+              <TouchableOpacity style={styles.button} onPress={this.showStartTimePicker}>
+                <Icon style={styles.dateIcon} name="calendar" size={40} color='white' />
+              </TouchableOpacity>
+              <DateTimePicker
+                cancelTextIOS={'Exit'}
+                confirmTextIOS={'OK'}
+                cancelTextStyle={{color: 'red', fontSize: 20}}
+                confirmTextStyle={{color: 'blue', fontSize: 20}}
+                isVisible={this.state.isStartTimePickerVisible}
+                onConfirm={this.handleStartDatePicked}
+                onCancel={this.hideStartTimePicker}
+                mode={'datetime'}
+                is24Hour={true}
+              />
+            </View>
+            <View style={styles.dateContainer}>
+              <View style={styles.dateTextContainer}>
+                <Text style={styles.dateText}>End Time</Text>
+                <Text style={styles.dateInnerText}>{this.state.pickupEndTime.toString().slice(4,21)}</Text>
+              </View>
+              <TouchableOpacity style={styles.button} onPress={this.showEndTimePicker}>
+                <Icon style={styles.dateIcon} name="calendar" size={40} color='white' />
+              </TouchableOpacity>
+              <DateTimePicker
+                cancelTextIOS={'Exit'}
+                confirmTextIOS={'OK'}
+                cancelTextStyle={{color: 'red', fontSize: 20}}
+                confirmTextStyle={{color: 'blue', fontSize: 20}}
+                isVisible={this.state.isEndTimePickerVisible}
+                onConfirm={this.handleEndDatePicked}
+                onCancel={this.hideEndTimePicker}
+                mode={'datetime'}
+                is24Hour={false}
+              />
+            </View>
           </View>
 
           <Text style={styles.errorTextStyle}>{this.state.error}</Text>
@@ -169,7 +229,7 @@ const styles = StyleSheet.create({
       backgroundColor: '#FFFFFF',
       borderRadius:30,
       borderBottomWidth: 1,
-      width:300,
+      width:350,
       height:45,
       marginBottom:20,
       flexDirection: 'row',
@@ -180,6 +240,7 @@ const styles = StyleSheet.create({
       marginLeft:16,
       borderBottomColor: '#1E1E1E',
       flex:1,
+      fontFamily: 'Futura',
   },
   inputIcon:{
     width:30,
@@ -202,26 +263,56 @@ const styles = StyleSheet.create({
   loginText: {
     color: 'white',
     fontWeight: 'bold',
+    justifyContent: 'center',
+    fontFamily: 'Futura'
   },
   errorTextStyle: {
     fontSize: 12,
     alignSelf: 'center',
     color: 'blue',
-    padding: 5
+    padding: 5,
+    fontFamily: 'Futura'
+  },
+  outsideDateContainer: {
+    flexDirection: 'row',
+  },
+  dateContainer: {
+    flex: 1,
+    // alignItems: 'center',
+    width: 350,
+    height: 120,
+  },
+  dateTextContainer: {
+    flexDirection: 'column',
   },
   button: {
-    width: 250,
+    width: 50,
     height: 50,
-    backgroundColor: 'blue',
+    backgroundColor: 'black',
     borderRadius: 30,
     justifyContent: 'center',
     marginTop: 15,
+    position: 'absolute',
+    right: 0,
   },
-  dateText: {
-    fontSize: 18,
+  dateIcon: {
     color: 'white',
     textAlign: 'center',
-  }
+  },
+  dateText: {
+    fontFamily: 'Futura',
+    paddingBottom: 15,
+    paddingLeft: 20,
+    paddingTop: 20,
+    paddingRight: 20,
+    fontWeight: 'bold',
+  },
+  dateInnerText: {
+    // justifyContent: 'center',
+    fontFamily: 'Futura',
+    paddingTop: 25,
+    padding:10,
+  },
 });
 
 export { AddDonation };
